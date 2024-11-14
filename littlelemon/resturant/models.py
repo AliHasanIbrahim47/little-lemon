6de4a5f05e7606ac8df4087e3a37f84b6daa3827
AliHasanIbrahim47 <ew9361903@gmail.com>
2024-11-14 16:59:27 +0300

    first commit

--- littlelemon/resturant/models.py
+++ littlelemon/resturant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=200)
+    no_of_guests= models.IntegerField(default=0)
+    booking_date = models.DateField()
+
+    def __str__(self): 
+        return self.name
+
+# Add code to create Menu model
+class Menu(models.Model):
+    title = models.CharField(max_length=200) 
+    price = models.IntegerField(null=False) 
+    inventory = models.IntegerField(default=0)
+    #menu_item_description = models.TextField(max_length=1000, default='') 
+
+    def __str__(self):
+        return self.title
