cba9d279e9b057767c32a57eb78422c4f7f0ca66
AliHasanIbrahim47 <ew9361903@gmail.com>
2024-11-17 04:04:39 +0300

    last commit

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,8 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'resturant'
+    # added apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser', 
+    'resturant',
 ]
+
+DJOSER = {
+    "USER_ID_FIELD" : "username"
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,3 +143,26 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES' : [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '100/minute',
+    }
+}
--- littlelemon/resturant/models.py
+++ littlelemon/resturant/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
-# Create your models here.
-class Booking(models.Model):
-    name = models.CharField(max_length=200)
-    no_of_guests= models.IntegerField(default=0)
-    booking_date = models.DateField()
-
-    def __str__(self): 
-        return self.name
-
-# Add code to create Menu model
 class Menu(models.Model):
-    title = models.CharField(max_length=200) 
-    price = models.IntegerField(null=False) 
-    inventory = models.IntegerField(default=0)
-    #menu_item_description = models.TextField(max_length=1000, default='') 
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
 
-    def __str__(self):
-        return self.title
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menu Items'
+
+    def __str__(self) -> str:
+        return f'{self.title} : {str(self.price)}'
+    
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField(default=0)
+    booking_date =models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Booking Records'
+
+    def __str__(self) -> str:
+        return f'{self.name} for {self.number_of_guests} guests on {self.booking_date}'
+
